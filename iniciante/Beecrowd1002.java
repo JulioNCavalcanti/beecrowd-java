86cf8aa6ee3308f8d30aae7619c1526d117d8eb9
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-08-12 15:13:27 -0300

    Adicionando Beecrowd1002.java

---
+++ iniciante/Beecrowd1002.java
@@ -0,0 +1,19 @@
+package iniciante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Beecrowd1002 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double raio = sc.nextDouble();
+        double n = 3.14159;
+        double area = n * Math.pow(raio, 2);
+
+        System.out.println("A=" + String.format("%.4f", area));
+
+        sc.close();
+    }
+}
